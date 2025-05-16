aace0ccb87b074870976e692792e7247c087cb9d
Mahesh Kumar <ml4909854@gmail.com>
2025-05-16 19:42:55 +0530

    do

---
+++ javascript/ternaryOperator/index.js
@@ -0,0 +1,39 @@
+
+
+
+// let num = 5
+// let result = (num%2===0)?"Even":"odd"
+// console.log(result)
+
+
+// let a = 10 
+// let b = 20
+// let result = (a>b)?a:b
+// console.log(result)
+
+
+// let year = 2020
+// let result = (year%400==0 || (year%4==0 && year%100 !=0))?"Leap year":"Not leap year"
+// console.log(result)
+
+// let char  = "e"
+// let result = ("aeiouAEIOU".includes(char))?"Vowel":"Consonant"
+// console.log(result)
+
+// let marks = 85
+// let grade = (marks>=98)?"A":(marks>=80)? "B":(marks>=70)?"C":(marks>=60)?"D":"F"
+// console.log(grade)
+
+
+
+// let num = -5
+// let result = (num>0) ?"Positive":(num==0)?"Zero":"Negative"
+// console.log(result)
+
+// let str = ""
+// let result = (str.length == 0)?"Empty":"Not empty"
+// console.log(result)
+
+let age = 19
+let result = (age>=18)?"Aligible to Vote":"Not eleible"
+console.log(result)
