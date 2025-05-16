c48d49bbe1e8a3e845a6edfdd471949b26eef231
Mahesh Kumar <ml4909854@gmail.com>
2025-05-16 15:27:21 +0530

    coding

---
+++ javascript/string/index3.js
@@ -0,0 +1,13 @@
+
+
+
+function capitalizeWords(str){
+let array = str.split(" ")
+
+for(let i = 0 ; i<=array.length; i++){
+    return array[i]
+}
+}
+
+let x = capitalizeWords("hello world")
+console.log(x)
