d358daade19a4c34b8b51211ee110986de0d246b
Mahesh Kumar <ml4909854@gmail.com>
2025-05-16 19:28:50 +0530

    coding

---
+++ javascript/Dates/index.js
@@ -0,0 +1,11 @@
+
+const d = new Date()
+
+let year = d.getFullYear()
+let month = d.getMonth()
+let date = d.getDate()
+
+const mon =  ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+console.log(year , mon[month] , date)
