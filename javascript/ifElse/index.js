ddbd73fae73e05f4f9a03796731fde9e61359cc2
Mahesh Kumar <ml4909854@gmail.com>
2025-05-17 13:31:13 +0530

    coding

---
+++ javascript/ifElse/index.js
@@ -0,0 +1,16 @@
+
+
+let marks = 95
+
+if(marks>90 && (marks<100)){
+    console.log("A")
+}else if ((marks>80) && (marks<90)){
+    console.log("B")
+}else if ((marks>70) && (marks<80)){
+    console.log("c")
+}else if (marks >100){
+    console.log("Marks cannot be greater than 100")
+
+}else{
+    console.log("F")
+}
