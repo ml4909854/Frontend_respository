cd314c28d886d8bde95da2870cccecf803808f4f
Mahesh Kumar <ml4909854@gmail.com>
2025-05-17 13:43:24 +0530

    d

---
+++ javascript/ifElse/index3.js
@@ -0,0 +1,22 @@
+
+
+
+let operator = "+"
+let num1 = 5
+let num2 = 10
+
+switch (operator) {
+    case "+":
+        console.log(num1+num2)
+        break;
+case "*":
+    console.log(num1*num2)
+    break;
+
+    case "-":
+        console.log(num1-num2)
+        break
+    default:
+        console.log("Error in output")
+        break;
+}
