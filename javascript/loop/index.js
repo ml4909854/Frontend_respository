c85e3d307ec32cb4c2997cba661756a9a3c718b2
Mahesh Kumar <ml4909854@gmail.com>
2025-05-17 14:02:33 +0530

    doc

---
+++ javascript/loop/index.js
@@ -0,0 +1,7 @@
+// sum of the first N natural Numbers
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+  sum += i;
+}
+
+console.log(sum)
---
+++ javascript/loop/index1.js
@@ -0,0 +1,8 @@
+
+
+// print multiplication table
+
+let table = 5
+for(let i = 1 ; i<=10; i++){
+    console.log(`${table} * ${i} = ${table*i}` )
+}
