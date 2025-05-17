878db70309821c16665a41429d67478f4e1fad20
Mahesh Kumar <ml4909854@gmail.com>
2025-05-17 14:07:06 +0530

    doc

---
+++ javascript/loop/index2.js
@@ -0,0 +1,9 @@
+
+
+
+const person = {name:"Alice" , age:25 , job:"Enginer"}
+let count = 0
+for(let x in person){
+    count++
+}
+console.log(count)
---
+++ javascript/loop/index3.js
@@ -0,0 +1,13 @@
+
+
+//
+
+const person = {
+    firstname:"Mahesh",
+    lastname:"Kumar",
+    age:23
+}
+
+for(let x in person){
+    console.log(x , person[x])
+}
