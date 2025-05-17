3d369c29f0c43d47a1394485586bd13cf2cd769b
Mahesh Kumar <ml4909854@gmail.com>
2025-05-17 13:46:11 +0530

    d

---
+++ javascript/ifElse/index4.js
@@ -0,0 +1,14 @@
+
+
+
+let age = 18
+
+if(age>0 && age<=12){
+console.log("child")
+}else if(age>13 && age<=19){
+    console.log("teenager")
+}else if (age>20 && age<=59){
+    console.log("Adult")
+}else{
+    console.log("senoir")
+}
