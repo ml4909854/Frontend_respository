fa83779abdaf3169cdfa5c53be1e211d3e6c3de1
Mahesh Kumar <ml4909854@gmail.com>
2025-05-20 13:38:05 +0530

    DsA

--- javascript/React/src/App.jsx
+++ javascript/React/src/App.jsx
@@ -1,82 +1,103 @@
-import React from "react";
+// import React from "react";
 
-// function App(){
-//   return <h1>HI I am App Component</h1>
-// }
+// // function App(){
+// //   return <h1>HI I am App Component</h1>
+// // }
 
-// // props in a component
-// function App(props){
-//   console.log(props.color , props.fontsize)
-// return <h1 style={{color:props.color , fontSize:props.fontsize}}>{props.color}</h1>
+// // // props in a component
+// // function App(props){
+// //   console.log(props.color , props.fontsize)
+// // return <h1 style={{color:props.color , fontSize:props.fontsize}}>{props.color}</h1>
 
-// }
+// // }
 
-// components in components
+// // components in components
 
-// function App(){
+// // function App(){
 
-//   return<>
-//   <h1>I am a App component</h1>
-//   <Car/>
-//   </>
-// }
-// function Car(){
-//   return <h1> I am a car Component</h1>
-// }
+// //   return<>
+// //   <h1>I am a App component</h1>
+// //   <Car/>
+// //   </>
+// // }
+// // function Car(){
+// //   return <h1> I am a car Component</h1>
+// // }
 
-// const import React from 'react'
+// // const import React from 'react'
 
-// const Garage = (props)=>{
-//   return <>
-//   <h1>I have a {props.car} car</h1>
+// // const Garage = (props)=>{
+// //   return <>
+// //   <h1>I have a {props.car} car</h1>
 
-//   </>
-// }
+// //   </>
+// // }
 
-// const  App= () => {
-//   let carname = "maruti 800"
-//   return <>
-//   <h1>hI aam a car component</h1>
-// <Garage car={carname}/>
-//   </>
-// };
+// // const  App= () => {
+// //   let carname = "maruti 800"
+// //   return <>
+// //   <h1>hI aam a car component</h1>
+// // <Garage car={carname}/>
+// //   </>
+// // };
 
-// /// react Events
+// // /// react Events
 
-// function App() {
-//   const shoot = (a) => {
-//     alert(a);
-//   };
+// // function App() {
+// //   const shoot = (a) => {
+// //     alert(a);
+// //   };
+// //   return (
+// //     <>
+// //       <button onClick={() => shoot("Great Shot!")}>Hit A Shot</button>
+// //     </>
+// //   )
+// // }
+// // import React from 'react'
+
+// const App = () => {
+
+// function Garage(props) {
+//   const cars = props.cars;
 //   return (
 //     <>
-//       <button onClick={() => shoot("Great Shot!")}>Hit A Shot</button>
+//       <h1>Garage</h1>
+//       {cars.length > 0 &&
+//         <h2>
+//           You have {cars.length} cars in your garage.
+//         </h2>
+//       }
 //     </>
+//   );
+// }
+ 
+// const cars = [
+//   // "ford" , "mustang" , "Maruti*900"
+// ]
+//   return (
+//     <div>
+//       <Garage cars={cars}/>
+//     </div>
 //   )
 // }
-// import React from 'react'
 
-const App = () => {
+// export default App
 
-function Garage(props) {
-  const cars = props.cars;
-  return (
-    <>
-      <h1>Garage</h1>
-      {cars.length > 0 &&
-        <h2>
-          You have {cars.length} cars in your garage.
-        </h2>
-      }
-    </>
-  );
-}
- 
-const cars = [
-  // "ford" , "mustang" , "Maruti*900"
-]
+
+import React from 'react'
+
+const App = (props) => {
+
+  function MissedGoal(){
+    return <h1>Goal is missed</h1>
+  }
+  function MadeGoal(){
+    return <h1>Goad is maded</h1>
+  }
+
   return (
     <div>
-      <Garage cars={cars}/>
+      {props.isGoal?<MadeGoal/> : <MissedGoal/>}
     </div>
   )
 }
