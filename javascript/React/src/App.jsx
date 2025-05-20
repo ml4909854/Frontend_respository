75ef7b3a2a1497aa0f0af97b7b569c544a1435b4
Mahesh Kumar <ml4909854@gmail.com>
2025-05-20 13:04:26 +0530

    DsA

--- javascript/React/src/App.jsx
+++ javascript/React/src/App.jsx
@@ -5,11 +5,24 @@ import React from 'react'
 // }
 
 
-// props in a component
-function App(props){
-  console.log(props.color , props.fontsize)
-return <h1 style={{color:props.color , fontSize:props.fontsize}}>{props.color}</h1>
+// // props in a component
+// function App(props){
+//   console.log(props.color , props.fontsize)
+// return <h1 style={{color:props.color , fontSize:props.fontsize}}>{props.color}</h1>
   
+// }
+
+// components in components
+
+function App(){
+
+  return<>
+  <h1>I am a App component</h1>
+  <Car/>
+  </> 
+}
+function Car(){
+  return <h1> I am a car Component</h1>
 }
 
 export default App
