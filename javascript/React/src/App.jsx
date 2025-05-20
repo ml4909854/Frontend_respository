45eab12607ab4623447c40c7d6b25caf27879676
Mahesh Kumar <ml4909854@gmail.com>
2025-05-20 13:19:27 +0530

    DsA

--- javascript/React/src/App.jsx
+++ javascript/React/src/App.jsx
@@ -1,28 +1,46 @@
-import React from 'react'
+import React from "react";
 
 // function App(){
 //   return <h1>HI I am App Component</h1>
 // }
 
-
 // // props in a component
 // function App(props){
 //   console.log(props.color , props.fontsize)
 // return <h1 style={{color:props.color , fontSize:props.fontsize}}>{props.color}</h1>
-  
+
 // }
 
 // components in components
 
-function App(){
+// function App(){
 
-  return<>
-  <h1>I am a App component</h1>
-  <Car/>
-  </> 
-}
-function Car(){
-  return <h1> I am a car Component</h1>
+//   return<>
+//   <h1>I am a App component</h1>
+//   <Car/>
+//   </>
+// }
+// function Car(){
+//   return <h1> I am a car Component</h1>
+// }
+
+// const import React from 'react'
+
+const Garage = (props)=>{
+  return <>
+  <h1>I have a {props.car} car</h1>
+  
+  </>
 }
 
-export default App
+const  App= () => {
+  let carname = "maruti 800"
+  return <>
+  <h1>hI aam a car component</h1>
+<Garage car={carname}/>
+  </>
+};
+
+
+
+export default App;
